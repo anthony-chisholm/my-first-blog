d93237513f4cdd8e0100580b843edd05bbb53c21
Anthony <admin@admin.com>
2017-06-16 09:34:42 +1000

    all done

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,16 @@ from django.utils import timezone
 from .models import Post
 from .forms import PostForm
 from django.shortcuts import redirect
+from django.http import JsonResponse
+from django.core import serializers
+
+def spa_post_list(request):
+    return render(request, 'blog/spa_post_list.html', {})
+
+def json_all_posts(request):
+    posts = Post.objects.order_by('published_date')
+    data = serializers.serialize('json', posts)
+    return JsonResponse(data, safe=False)
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
